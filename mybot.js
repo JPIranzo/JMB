d900b1ccdc1ad0205c3842ba09d05e786550ddf4
Jaime Palomo Iranzo <jaimepalomo16@gmail.com>
2019-02-11 11:23:37 +0100

    upload files

---
+++ mybot.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+//send message to log when online
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+/* Message code
+client.on("message", message => {
+	if (message.author.bot) return;
+  // This is where we'll put our code.
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  switch (command) {
+  	case "ping" :
+  	message.channel.send('Pong!');
+  	break;
+  	case "blah" :
+  	message.channel.send('Meh.');
+  	break;
+  }
+});
+*/
+//Reaction code
+client.on("messageReactionAdd", (reaction, user) => {
+	if (reaction.emoji.identifier===config.quote){
+		reaction.message.channel.send(user.toString() + ' ha citado el mensaje de ' + reaction.message.author.toString() + ' :\n ```' + reaction.message.content + '```');
+}
+});
+
+client.login(config.token);
